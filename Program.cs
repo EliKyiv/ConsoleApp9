5b44dfe0e6fd9aec144736c216f939cff320d7b6
INFOPULSE\Illia.Linkov <illia.linkov@infopulse.com>
2023-04-17 16:36:06 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+delegate void PrintString(string input);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PrintString greenPrinter = PrintInGreen;
+        PrintString orangePrinter = PrintInOrange;
+
+        greenPrinter("Hello, world!");
+        orangePrinter("Greetings, earthling!");
+    }
+
+    static void PrintInGreen(string input)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(input);
+        Console.ResetColor();
+    }
+
+    static void PrintInOrange(string input)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine(input);
+        Console.ResetColor();
+    }
+}
